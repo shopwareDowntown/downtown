d321c15b8cd30fdc91efcc51718318a6d1fd03be
Soner Sayakci <s.sayakci@shopware.com>
2020-03-26 18:01:40 +0100

    Add shipping method limitation

--- src/Merchants/Content/Merchant/Services/CartValidator.php
+++ src/Merchants/Content/Merchant/Services/CartValidator.php
@@ -5,13 +5,13 @@ namespace Shopware\Production\Merchants\Content\Merchant\Services;
 use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Cart\CartValidatorInterface;
 use Shopware\Core\Checkout\Cart\Error\ErrorCollection;
-use Shopware\Core\Checkout\Cart\Error\IncompleteLineItemError;
 use Shopware\Core\Content\Product\ProductEntity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use Shopware\Core\Framework\DataAbstractionLayer\Exception\InconsistentCriteriaIdsException;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Production\Merchants\Content\Merchant\Exception\CartContainsMultipleMerchants;
+use Shopware\Production\Merchants\Content\Merchant\Exception\CartInvalidShippingMethod;
+use Shopware\Production\Merchants\Content\Merchant\MerchantEntity;
 
 class CartValidator implements CartValidatorInterface
 {
@@ -35,7 +35,7 @@ class CartValidator implements CartValidatorInterface
 
         $criteria = new Criteria($ids);
 
-        $criteria->addAssociation('merchants');
+        $criteria->addAssociation('merchants.shippingMethods');
 
         $products = $this->productRepository->search($criteria, $salesChannelContext->getContext());
 
@@ -52,13 +52,37 @@ class CartValidator implements CartValidatorInterface
             }
 
             $merchantId = $product->getExtension('merchants')->first()->getId();
+            /** @var MerchantEntity $merchant */
+            $merchant = $product->getExtension('merchants')->first();
 
-            $merchantIds[$merchantId] = true;
+            $merchantIds[$merchant->getId()] = true;
 
             if (count($merchantIds) > 1) {
                 $errorCollection->add(new CartContainsMultipleMerchants($lineItem->getId()));
                 $cart->getLineItems()->removeElement($lineItem);
+            } else {
+                $shippingMethodId = $cart->getDeliveries()->first()->getShippingMethod()->getId();
+                $this->blockOrderWithInvalidShippingMethod($shippingMethodId, $errorCollection, $merchant, $salesChannelContext);
             }
         }
+    }
+
+    private function blockOrderWithInvalidShippingMethod(
+        string $activeId,
+        ErrorCollection $errorCollection,
+        MerchantEntity $merchantEntity,
+        SalesChannelContext $context
+    ): void
+    {
+        // Default shipping method
+        if ($activeId === $context->getSalesChannel()->getShippingMethodId()) {
+            return;
+        }
+
+        if ($merchantEntity->getShippingMethods()->has($activeId)) {
+            return;
+        }
+
+        $errorCollection->add(new CartInvalidShippingMethod());
     }
 }
