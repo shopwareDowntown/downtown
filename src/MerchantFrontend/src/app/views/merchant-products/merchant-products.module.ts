29781f4c4ed5aed11b1ca7e0976bc7fc01db93ed
Andreas Wolf <a.wolf@shopware.com>
2020-03-26 19:37:51 +0100

    add first skeleton for product edit

--- src/MerchantFrontend/src/app/views/merchant-products/merchant-products.module.ts
+++ src/MerchantFrontend/src/app/views/merchant-products/merchant-products.module.ts
@@ -5,6 +5,7 @@ import { MerchantProductsRoutingModule } from './merchant-products-routing.modul
 import { MerchantProductsListingComponent } from './merchant-products-listing/merchant-products-listing.component';
 import { MerchantProductsDetailComponent } from './merchant-products-detail/merchant-products-detail.component';
 import {ClrDatagridModule, ClrFormsModule, ClrIconModule} from '@clr/angular';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import {ClrDatagridModule, ClrFormsModule, ClrIconModule} from '@clr/angular';
   imports: [
     CommonModule,
     MerchantProductsRoutingModule,
+    FormsModule,
 
     // Clarity
     ClrDatagridModule,
