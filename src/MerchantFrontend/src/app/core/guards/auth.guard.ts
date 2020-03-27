b1f8044dac227f3aec6d2c1cd9be8098be64098b
Arnold Stoba <me@arnoldstoba.com>
2020-03-27 15:42:56 +0100

    Revert auth guard change

--- src/MerchantFrontend/src/app/core/guards/auth.guard.ts
+++ src/MerchantFrontend/src/app/core/guards/auth.guard.ts
@@ -14,9 +14,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    return true;
-    // return this.stateService.isLoggedIn();
+    return this.stateService.isLoggedIn();
   }
 
 }
