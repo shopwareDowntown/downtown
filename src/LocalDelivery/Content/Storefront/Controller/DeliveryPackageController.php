de6837e66ccefde6f36600b1475486291923e7e8
Jakob Kruse <j.kruse@shopware.com>
2020-03-27 14:08:55 +0100

    Add package List for delivery boy

---
+++ src/LocalDelivery/Content/Storefront/Controller/DeliveryPackageController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Shopware\Production\LocalDelivery\Content\Storefront\Controller;
+
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+/**
+ * @RouteScope(scopes={"storefront"})
+ */
+class DeliveryPackageController extends StorefrontController
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+
+    public function __construct(
+        Environment $twig
+    )
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route(name="storefront.delivery-packages", path="/delivery-packages")
+     */
+    public function packages(Request $request): Response
+    {
+        $packages = [
+            [
+                'status' => 'ok cool',
+                'zipCode' => "23333",
+                'city' => "hamburg",
+                'street' => "abc 123",
+                'content' => 'gunther'
+            ],
+            [
+                'status' => 'ok nich so cool',
+                'zipCode' => "69420",
+                'city' => "kekistan",
+                'street' => "moin joachim 14",
+                'content' => 'Frühlingsrollen'
+            ]
+        ];
+
+        $html = $this->renderStorefront('@LocalDelivery/storefront/page/packages/index.html.twig', [
+            'packages' => $packages
+        ]);
+        return new Response($html);
+    }
+
+}
