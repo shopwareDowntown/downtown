36b67a1348b6a5c1ecc8cdc687b2a504af01870c
Soner Sayakci <s.sayakci@shopware.com>
2020-03-30 16:46:43 +0200

    Redirect to merchant portal after confirming account, fixes #171

--- src/Merchants/Content/Merchant/Storefront/Controller/DoubleOptinController.php
+++ src/Merchants/Content/Merchant/Storefront/Controller/DoubleOptinController.php
@@ -2,26 +2,22 @@
 
 namespace Shopware\Production\Merchants\Content\Merchant\Storefront\Controller;
 
-use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Customer\Exception\CustomerAlreadyConfirmedException;
 use Shopware\Core\Checkout\Customer\Exception\CustomerNotFoundByHashException;
 use Shopware\Core\Checkout\Customer\SalesChannel\AccountRegistrationService;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\Framework\Validation\DataBag\QueryDataBag;
-use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\Framework\Validation\Exception\ConstraintViolationException;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Production\Merchants\Content\Merchant\Services\RegistrationService;
 use Shopware\Storefront\Controller\StorefrontController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @RouteScope(scopes={"storefront"})
  */
-class RegistrationController extends StorefrontController
+class DoubleOptinController extends StorefrontController
 {
     /**
      * @var AccountRegistrationService
@@ -44,20 +40,9 @@ class RegistrationController extends StorefrontController
         } catch (CustomerNotFoundByHashException | CustomerAlreadyConfirmedException | ConstraintViolationException $exception) {
             $this->addFlash('danger', $this->trans('account.confirmationIsAlreadyDone'));
 
-            return $this->redirectToRoute('frontend.home.page');
+            return new RedirectResponse(getenv('MERCHANT_PORTAL'));
         }
 
-        $this->addFlash('success', $this->trans('account.doubleOptInRegistrationSuccessfully'));
-
-        return $this->redirectToRoute('frontend.home.page');
-    }
-
-    protected function addFlash(string $type, string $message)
-    {
-        if (!$this->container->has('session')) {
-            throw new \LogicException('You can not use the addFlash method if sessions are disabled. Enable them in "config/packages/framework.yaml".');
-        }
-
-        $this->container->get('session')->getFlashBag()->add($type, $message);
+        return new RedirectResponse(getenv('MERCHANT_PORTAL') . '?merchantRegistrationCompleted=1');
     }
 }
