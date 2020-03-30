114cb86f9e33990bf12b036fb427b22cea19eb1d
Andreas Wolf <a.wolf@shopware.com>
2020-03-30 08:25:34 +0200

    use switchMap instead of two subscribes

--- src/MerchantFrontend/src/app/views/merchant-products/merchant-products-detail/merchant-products-detail.component.ts
+++ src/MerchantFrontend/src/app/views/merchant-products/merchant-products-detail/merchant-products-detail.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../core/models/product.model';
 import { MerchantApiService } from '../../../core/services/merchant-api.service';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'portal-merchant-products-detail',
@@ -13,12 +15,18 @@ export class MerchantProductsDetailComponent {
   product: Product = null;
 
   constructor(private activeRoute: ActivatedRoute, private merchantApiService: MerchantApiService) {
-    // Get resolved product from route
-    this.activeRoute.params.subscribe(value => {
-      if (value && value.id) {
-        this.merchantApiService.getProduct(value.id).subscribe((product: { data:Product}) => {
-          this.product = product.data;
-        });
+    this.activeRoute.params.pipe(
+      switchMap((value) => {
+          if (value && value.id) {
+            return this.merchantApiService.getProduct(value.id);
+          } else {
+            return of(false);
+          }
+        }
+      )
+    ).subscribe((product: { data: Product }) => {
+      if(product && product.data) {
+        this.product = product.data;
       } else {
         this.product = <Product>{
           name: '',
@@ -28,7 +36,6 @@ export class MerchantProductsDetailComponent {
           tax: 19
         }
       }
-
     });
   }
 
@@ -38,7 +45,7 @@ export class MerchantProductsDetailComponent {
         this.product = product;
       });
     } else {
-      this.merchantApiService.addProduct(this.product).subscribe((product: { data:Product}) => {
+      this.merchantApiService.addProduct(this.product).subscribe((product: { data: Product }) => {
         this.product = product.data;
       });
     }
