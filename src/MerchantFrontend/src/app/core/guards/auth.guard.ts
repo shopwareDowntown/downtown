ac7c2a3add5d31dce4fba19e3f18bb64b9b1232a
Andreas Wolf <a.wolf@shopware.com>
2020-03-31 18:00:35 +0200

    fix auth guard

--- src/MerchantFrontend/src/app/core/guards/auth.guard.ts
+++ src/MerchantFrontend/src/app/core/guards/auth.guard.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { StateService } from '../state/state.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private readonly stateService: StateService) {
+  constructor(private router: Router, private readonly stateService: StateService) {
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.stateService.isLoggedIn();
-  }
 
+    return this.stateService.isLoggedIn()
+      .pipe(
+        map((isLoggedIn: boolean) => {
+          if (!isLoggedIn) {
+            return this.router.createUrlTree(['']);
+          }
+          return isLoggedIn;
+        })
+      );
+  }
 }
