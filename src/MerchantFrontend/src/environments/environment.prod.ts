24016872e4b288840b687b3690d05880e5e05d7e
Andreas Wolf <a.wolf@shopware.com>
2020-04-01 11:07:29 +0200

    add Prod configuration

--- src/MerchantFrontend/src/environments/environment.prod.ts
+++ src/MerchantFrontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   hubspotPortalId: '6506294',
   hubspotFormId: 'e2d4a12e-a1c0-4ac0-87b5-ed89c490ecff',
-  apiUrl: '',
-  accessKey: ''
+  apiUrl: 'https://api.downtowns.io',
+  accessKey: 'SWSCPY4MIQDUPU3IFWDX29USYW'
 };
