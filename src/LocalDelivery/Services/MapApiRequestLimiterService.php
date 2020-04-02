622af3d3fe9e81da13d077dc2115c7663372c881
Sebastian Seggewiß <s.seggewiss@shopware.com>
2020-04-02 06:32:53 +0200

    Move strict type decleration to first file line

--- src/LocalDelivery/Services/MapApiRequestLimiterService.php
+++ src/LocalDelivery/Services/MapApiRequestLimiterService.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 /*
  * (c) shopware AG <info@shopware.com>
  * For the full copyright and license information, please view the LICENSE
