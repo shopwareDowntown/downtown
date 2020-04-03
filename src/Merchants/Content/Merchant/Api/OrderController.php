9021fdd4c8edcd94f074655c1ba797a286d04e2c
Soner Sayakci <s.sayakci@shopware.com>
2020-04-03 17:30:49 +0200

    Fix order endpoint to list orders as array

--- src/Merchants/Content/Merchant/Api/OrderController.php
+++ src/Merchants/Content/Merchant/Api/OrderController.php
@@ -73,7 +73,7 @@ class OrderController
         $orders = $this->orderRepository->search($criteria, Context::createDefaultContext());
 
         return new JsonResponse([
-            'data' => $orders->getElements(),
+            'data' => $orders->getEntities(),
             'total' => $orders->getTotal()
         ]);
     }
