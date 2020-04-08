1f5a4fbd75022f5a6e4e477ac6ed23e2b1f8c512
Soner Sayakci <s.sayakci@shopware.com>
2020-04-08 11:09:52 +0200

    Fix new bundle integration

--- src/Merchants/Checkout/Cart/Subscriber/OrderPlacedSubscriber.php
+++ src/Merchants/Checkout/Cart/Subscriber/OrderPlacedSubscriber.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Production\Merchants\Checkout\Subscriber;
+namespace Shopware\Production\Merchants\Checkout\Cart\Subscriber;
 
 use Shopware\Core\Checkout\Cart\Event\CheckoutOrderPlacedEvent;
 use Shopware\Core\Content\Product\ProductEntity;
