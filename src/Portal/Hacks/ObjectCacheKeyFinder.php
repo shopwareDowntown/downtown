2e34ff5b921272d69d74961359fc6a4dadf36e60
Soner Sayakci <s.sayakci@shopware.com>
2020-04-09 10:07:54 +0200

    Fix http cache

---
+++ src/Portal/Hacks/ObjectCacheKeyFinder.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Production\Portal\Hacks;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Theme\ThemeEntity;
+
+/**
+ * This decoration fixes http cache issues while switching theme settings
+ * @see https://issues.shopware.com/issues/NEXT-8059
+ */
+class ObjectCacheKeyFinder extends \Shopware\Storefront\Framework\Cache\ObjectCacheKeyFinder
+{
+    /**
+     * @var ObjectCacheKeyFinder
+     */
+    private $core;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $salesChannelRepository;
+
+    public function __construct(\Shopware\Storefront\Framework\Cache\ObjectCacheKeyFinder $core, EntityRepositoryInterface $themeRepository)
+    {
+        $this->core = $core;
+        $this->salesChannelRepository = $themeRepository;
+    }
+
+    public function find(array $data, SalesChannelContext $context): array
+    {
+        $tags = $this->core->find($data, $context);
+        $theme = $this->fetchTheme($context->getSalesChannel()->getId());
+
+        if ($theme) {
+            $tags[] = 'theme-' . $theme->getId();
+        }
+
+        return $tags;
+    }
+
+    private function fetchTheme(string $id): ?ThemeEntity
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('salesChannels.id', $id));
+        return $this->salesChannelRepository->search($criteria, Context::createDefaultContext())->first();
+    }
+}
