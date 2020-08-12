2579df63dde1ec657f077f0a4f4a79bfb9903d0d
Soner Sayakci <s.sayakci@shopware.com>
2020-08-12 09:18:40 +0200

    Fix route scope in production

--- src/Portal/PortalBundle.php
+++ src/Portal/PortalBundle.php
@@ -3,6 +3,7 @@
 namespace Shopware\Production\Portal;
 
 use Shopware\Core\Framework\Bundle;
+use Shopware\Core\Framework\DependencyInjection\CompilerPass\RouteScopeCompilerPass;
 use Shopware\Storefront\Framework\ThemeInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -24,10 +25,30 @@ class PortalBundle extends Bundle implements ThemeInterface
         $loader->load('hacks.xml');
         $loader->load('services.xml');
         $this->registerMigrationPath($container);
+
+        $this->removeRouteScopePrefixCompilerPass($container);
     }
 
     public function getThemeConfigPath(): string
     {
         return 'theme.json';
+    }
+
+    /**
+     * Shopware Clod introduced prefixes for route scopes. Unfortunately that kills dependency injection of the route scopes
+     * @see https://github.com/shopware/platform/commit/2af081233fe8fcd78847ca2ea11a7137b74be047
+     *
+     * This method removes the core compiler pass
+     */
+    private function removeRouteScopePrefixCompilerPass(ContainerBuilder $container)
+    {
+        $config = $container->getCompilerPassConfig()->getBeforeOptimizationPasses();
+        foreach ($config as $key => $item) {
+            if (get_class($item) === RouteScopeCompilerPass::class) {
+                unset($config[$key]);
+            }
+        }
+
+        $container->getCompilerPassConfig()->setBeforeOptimizationPasses($config);
     }
 }
